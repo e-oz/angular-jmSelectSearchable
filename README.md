645c6d5bb556fef32cfcb2e765b3908870a6275a
OZ <normandiggs@gmail.com>
2013-09-10 16:45:21 +0400

    Readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-angular-jmSelectSearchable
+jmSelectSearchable
 ==========================
+###Directive to select from list of objects with search input box     
+It's not "typeahead", because in typeahead you can input any text in inputbox, and here you can only select from list.  
 
-Directive to select from list of objects with search input box
+###Attributes
+
+* **objects** - model which should contain list of objects to select
+* **ng-model** - model will be set on select
+* **print-attrs** - template for output attributes of object in list, attributes should be in curly braces (for example: "{{ first_name }}: {{ mobile }}") 
+
+###Example
+Included HTML template is an example, currently designed for Twitter Bootstrap 3.0.0    
+Usage [example in Plunker](http://plnkr.co/edit/UVzDiNZKbjDE1ZxY9hJD)  
+
---
+++ jmSelectSearchable.js
@@ -0,0 +1,38 @@
+angular.module('exampleApp')
+.directive('jmSelectSearchable', function() {
+							 return {
+								 templateUrl: 'selectSearchable.html',
+								 restrict:    'AE',
+								 scope:       {
+									 objects:  "=",
+									 ngModel:   "=",
+									 printAttrs: "@"
+								 },
+								 compile:     function(element, tAttrs) {
+									 tAttrs.printAttrs = tAttrs.printAttrs.replace(/{{/g, '[[');
+									 tAttrs.printAttrs = tAttrs.printAttrs.replace(/}}/g, ']]');
+								 },
+								 controller:  function($scope, $interpolate) {
+									 $scope.setObject = function(object) {
+										 $scope.ngModel = object;
+										 $scope.show_list = false;
+										 $scope.search = '';
+									 };
+
+									 $scope.getItemString = function(item) {
+										 if (angular.isUndefined(item)) {
+											 return '';
+										 }
+										 if (!$scope.printAttrs || !angular.isString($scope.printAttrs)) {
+											 $scope.printAttrs = '{{ first_name }} {{ last_name }}';
+										 }
+										 else {
+											 $scope.printAttrs = $scope.printAttrs.replace(/\[\[/g, '{{');
+											 $scope.printAttrs = $scope.printAttrs.replace(/\]\]/g, '}}');
+										 }
+										 return $interpolate($scope.printAttrs)(item);
+									 };
+								 }
+							 };
+						 });
+
